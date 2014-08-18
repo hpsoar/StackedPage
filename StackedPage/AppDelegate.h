0dec6faabaab23272785168c6037f30da37fc54a
hpsoar <hpsoar@gmail.com>
2014-08-18 11:32:12 +0800

    Initial Commit

---
+++ StackedPage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StackedPage
+//
+//  Created by HuangPeng on 8/18/14.
+//  Copyright (c) 2014 HuangPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
